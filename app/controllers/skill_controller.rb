3371f9854a5c48437133c00b51d9500b75f43aba
Faith Moraa <moraaf770@gmail.com>
2023-03-05 01:00:06 +0300

    Changes to skill file

--- app/controllers/skill_controller.rb
+++ app/controllers/skill_controller.rb
@@ -1,94 +1,51 @@
 class SkillController < AppController
 
-    set :views, './app/views'
+    set :views, '../views/project.erb'
 
-    # @method: Display a small welcome message
-    get '/hello' do
-        "Our very first controller"
-    end
 
-    # @method: Add a new TO-DO to the DB
-    post '/skill/create' do
+    post '/skills/create' do
+        data = JSON.parse(request.body.read)
+
         begin
-            skill = Skill.create( self.data(create: true) )
-            json_response(code: 201, data: skill)
+            skill = Skill.create(data)
+            skill.to_json
         rescue => e
-            json_response(code: 422, data: { error: e.message })
+            { error: e.message }.to_json
         end
+        
     end
 
-    # @method: Display all skills
-    get '/skill' do
-        skill = Skill.all
-        json_response(data: skills)
+    get '/skills' do 
+        skills = Skill.all
+        skills.to_json
     end
 
-    # @view: Renders an erb file which shows all skills
-    # erb has content_type because we want to override the default set above
-    get '/' do
-        @skills = Skill.all.map { |todo|
-          {
-            skill: skill,
-            badge: skill_status_badge(skill.status)
-          }
-        }
-        @i = 1
-        erb_response :skills
+   
+
+    # gets the sills of a certain user and limits them to max of 10
+    get '/skills/:id' do
+        user = User.find(self.user_id)
+        skillSet = user.skills.first(2)
+        skillSet.to_json
     end
 
-    # @method: Update existing TO-DO according to :id
-    put '/skill/update/:id' do
+    # adds a new skill for a specifies user
+    post 'skill/create' do
+        data = JSON.parse(request.body.read)
+
         begin
-            skill = Skill.find(self.skill_id)
-            skill.update(self.data)
-            json_response(data: { message: "skill updated successfully" })
+            skill = Skill.create(data)
+            skill.to_json
         rescue => e
-            json_response(code: 422 ,data: { error: e.message })
-        end
-    end
-
-    # @method: Delete TO-DO based on :id
-    delete '/skill/destroy/:id' do
-        begin
-            skill = Skill.find(self.skill_id)
-            skill.destroy
-            json_response(data: { message: "skill deleted successfully" })
-        rescue => e
-          json_response(code: 422, data: { error: e.message })
+            { error: e.message }.to_json
         end
     end
 
 
     private
 
-    # @helper: format body data
-    def data(create: false)
-        payload = JSON.parse(request.body.read)
-        if create
-            payload["createdAt"] = Time.now
-        end
-        payload
-    end
-
-    # @helper: retrieve to-do :id
-    def skill_id
+    def user_id
         params['id'].to_i
-    end
-
-    # @helper: format status style
-    def skill_status_badge(status)
-        case status
-            when 'CREATED'
-                'bg-info'
-            when 'ONGOING'
-                'bg-success'
-            when 'CANCELLED'
-                'bg-primary'
-            when 'COMPLETED'
-                'bg-warning'
-            else
-                'bg-dark'
-        end
     end
 
 
