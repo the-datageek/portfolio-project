7f268627988a189b95366caf266396cee9a68a12
Faith Moraa <moraaf770@gmail.com>
2023-03-05 01:07:15 +0300

    Update to user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
+# frozen_string_literal: true
+
 class User < ActiveRecord::Base
-    # table consists of password_hash as a column to store password hashes in DB
-    include BCrypt
-    # retrieve password from hash
-    def password
-      @password ||= Password.new(password_hash)
-    end
-    # create the password hash
-    def password=(new_pass)
-      @password = Password.create(new_pass)
-      self.password_hash = @password
-    end
+  has_many :projects
+  has_many :skills
+  # table consists of password_hash as a column to store password hashes in DB
+  include BCrypt
+
+  # retrieve password from hash
+  def password
+    @password ||= Password.new(password_hash)
   end
+
+  # create the password hash
+  def password=(pass)
+    @password = Password.create(pass)
+    self.password_hash = @password
+  end
+
+end
