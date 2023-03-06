13b57d4a27e32106a12e64f7ce0a09a71fce4b05
Faith Moraa <moraaf770@gmail.com>
2023-03-06 06:51:50 +0300

    Changes to skill controller

--- app/controllers/skill_controller.rb
+++ app/controllers/skill_controller.rb
@@ -1,52 +1,95 @@
 class SkillController < AppController
 
-    set :views, '../views/project.erb'
-
-
+    set :views, './app/views'
+  
+    # @method which will display a small welcome message
+    get '/hello' do
+      "You are in the Skill Controller go a head and add your skills"
+    end
+  
+    # @method: Adds a new created skill to the DB
     post '/skills/create' do
-        data = JSON.parse(request.body.read)
-
-        begin
-            skill = Skill.create(data)
-            skill.to_json
-        rescue => e
-            { error: e.message }.to_json
-        end
-        
+      begin
+        skill = Skill.create( self.data(create: true) )
+        json_response(code: 201, data: skill)
+      rescue => e
+        json_response(code: 422, data: { error: e.message })
+      end
     end
-
-    get '/skills' do 
-        skills = Skill.all
-        skills.to_json
+  
+  
+    # @method which displays all skills in our DB
+    get '/skills' do
+      skills = Skill.all
+      json_response(data: skills)
     end
-
-   
-
-    # gets the sills of a certain user and limits them to max of 10
-    get '/skills/:id' do
-        user = User.find(self.user_id)
-        skillSet = user.skills.first(2)
-        skillSet.to_json
+  
+    # @view that renders an erb file o/p if any and shows a list of all projects
+    # erb has content_type because we want to override the deafult set above
+    get '/' do
+      @skills = Skill.all.map { |skill|
+      {
+        skill: skill,
+        badge: skill_status_badge(Skill.status)
+      }
+      }
+      @i = 1
+      erb_response :skills
     end
-
-    # adds a new skill for a specifies user
-    post 'skill/create' do
-        data = JSON.parse(request.body.read)
-
-        begin
-            skill = Skill.create(data)
-            skill.to_json
-        rescue => e
-            { error: e.message }.to_json
-        end
+  
+    # @method which updates a specific skill using its :id as reference
+    put '/skills/update/:id' do
+      begin
+        skill = Skill.find(self.skill_id)
+        Skill.update(self.data)
+        json_response(data: { message: 'Skill updated successfully' })
+      rescue => e
+          json_response(code: 422, data: { error: e.message })
+      end
     end
-
-
+  
+    # @method that deletes a skill from the DB using its :id
+    delete '/skills/destroy/:id' do
+      begin
+        skill = Skill.find(self.skill_id)
+        Skill.destroy
+        json_response(data: { message: 'Skill deleted successfully' })
+      rescue => e
+        json_response(code: 422, data: { error: e.message })
+      end
+    end
+  
+  
     private
-
-    def user_id
-        params['id'].to_i
+  
+    # @helper: format body data to JSON format
+    def data(create: false)
+      payload = JSON.parse(request.body.read)
+      if create
+        payload['createdAt'] = Time.now
+      end
+      payload
     end
-
-
-end
+  
+    # @helper which assists in retreiving skill :id
+    def skill_id
+      params['id'].to_i
+    end
+  
+    # @helper that formats status style
+    def skill_status_badge(status)
+      case status
+      when 'CREATED'
+        'bg-success'
+      when 'ONGOING'
+        'bg-suu-success'
+      when 'CANCELLED'
+        'bg-primary'
+      when 'COMPLETED'
+        'bg-warning'
+      else
+        'bg-dark'
+      end
+    end
+  
+  end
