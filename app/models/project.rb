5bc670e50a0f2351cdae909d1a2560a5d48baab9
Faith Moraa <moraaf770@gmail.com>
2023-03-06 06:57:34 +0300

    Update on project.rb file

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 class Project < ActiveRecord::Base
-    
+    # active one to many association
     belongs_to :user
 
+    #restricting our integer valuese to be only betwween the specified Enum values
+    #our enums start from 0 to 3, 0 represnts CREATED and so on...
+    
     enum :status, [ :CREATED, :ONGOING, :COMPLETED, :CANCELLED ]
 end
